f1b6b9b5960f675a9dfeb125a874f46d6e4a8e15
esslmmm <http.esnonofficial@gmail.com>
2024-12-22 20:43:15 +0700

    CREATE HomeView

---
+++ dsl-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import HomeView from './view/HomeView.vue';
+
+</script>
+
+<template>
+  <Navbar/>
+  <HomeView/>
+
+</template>
---
+++ dsl-project/src/view/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import Hero from '@/components/Hero.vue'
+  import Notice from '@/components/Notice.vue';
+  import Service from '@/components/Service.vue';
+  import Notice_2 from '@/components/Notice_2.vue';
+  import Example from '@/components/Example.vue';
+  import Footer from '@/components/Footer.vue';
+</script>
+
+<template>
+     <Hero />  
+     <Notice/>
+     <Service/>
+     <Notice_2/>
+     <Example/>
+     <Footer/>
+</template>
